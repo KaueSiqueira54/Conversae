c6c65b076e389523fd150561005cfc903101cbff
KaueSiqueira <kaue54150@gmail.com>
2026-03-13 14:56:58 -0300

    Responsividade Mobile deita e alterações no desktop

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,1 +1,23 @@
-                                      
+// Menu Mobile
+
+const menuIcon = document.getElementById("menu_icon");
+const mobileMenu = document.getElementById("mobileMenu");
+const closeMenu = document.getElementById("closeMenu");
+
+// Abre o menu ao clicar no icone
+menuIcon.addEventListener("click", () => {
+  mobileMenu.classList.add("active");
+});
+
+// Fecha o menu
+closeMenu.addEventListener("click", () => {
+  mobileMenu.classList.remove("active");
+});
+
+// Fecha o menu ao clicar em qualquer link
+document.querySelectorAll(".mobile_nav_links a").forEach((link) => {
+  link.addEventListener("click", () => {
+    mobileMenu.classList.remove("active");
+  });
+});
+
